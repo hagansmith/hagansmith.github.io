472f9856bf4cf93fc64ab6f0033bedc64830ac30
Adam Smith <hagansmith@gmail.com>
2017-10-04 20:08:22 -0500

    added search bar back to blog page

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -1,24 +1,32 @@
 "use strict";
 
+let navBar = '';
 
-$('#header').html(`
-	<div class="jumbotron">
-	<h1 class ="text-center text-uppercase">Adam Smith</h1>
-	<nav class="text-center">
-			<li class= "btn btn-default nav"><a href="index.html">History</a></li>
-			<li class= "btn btn-default nav"><a href="contact.html">Contact</a></li>
-			<li class= "btn btn-default nav"><a href="about_me.html">About Me</a></li>
-			<li class= "btn btn-default nav"><a href="projects.html">Projects</a></li>
-			<li class= "btn btn-default nav"><a href="blog.html">Blog</a></li>
-	</nav>
-	</div>`);
+	navBar += `<div class="jumbotron">`;
+	navBar += `<h1 class ="text-center text-uppercase">Adam Smith</h1>`;
+	navBar += `<nav class="text-center">`;
+	navBar +=    `<li class= "btn btn-default"><a class="nav" href="index.html">History</a></li>`;
+	navBar +=    `<li class= "btn btn-default"><a class="nav" href="contact.html">Contact</a></li>`;
+	navBar +=    `<li class= "btn btn-default"><a class="nav" href="about_me.html">About Me</a></li>`;
+	navBar +=    `<li class= "btn btn-default"><a class="nav" href="projects.html">Projects</a></li>`;
+	navBar +=    `<li class= "btn btn-default"><a class="nav" href="blog.html">Blog</a></li>`;
+	navBar += `</nav>`;
+    if ($('title').html().split(" | ")[1] === "Blog") {
+  navBar += `<br>`;
+  navBar += `<label class="sr-only" for="blogSearch">Search Blog</label>`;
+  navBar += `<input type="text" id="blogSearch" class="form-control" placeholder="Search Blog">`;
+  }
+	navBar += `</div>`;
+
+$('#header').html(navBar);
 
 //add active class to selected page
 let nav = $('.nav');
   for (var i = 0; i < nav.length; i++) {
-    if (($('.nav').children()[i].innerText) === ($('title').html().split(" | ")[1])) {
+    // console.log($('.nav')[i].innerText);
+    if (($('.nav')[i].innerText) === ($('title').html().split(" | ")[1])) {
      let btn = $('.nav')[i];
-      $(btn).addClass('active');
+      $(btn).parent().addClass('active');
     }
   }
 
